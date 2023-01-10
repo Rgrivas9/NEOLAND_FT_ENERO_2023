a5648d001e48fb661d62af9331f601b6f9396d44
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-01-10 21:33:11 +0100

    iteracion5

--- loops/loops_iteracion1.js
+++ loops/loops_iteracion1.js
@@ -2,6 +2,4 @@
 const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta']
 for (p of products){
     if (p.toLowerCase().includes("camiseta")){
-        console.log(p)
-    }
-}
+        console.log(p)}};
---
+++ loops/prueba.js
@@ -0,0 +1,5 @@
+const x='hola gato'
+console.log(x.includes('gato'))
+const lis = [1,2,3,4]
+lis.reverse()
+console.log(lis)
