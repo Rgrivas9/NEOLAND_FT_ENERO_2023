829a390836eae7ac4ab9a7a62b390804b0b0e8df
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-01-10 21:42:28 +0100

    iteracion6

--- loops/loops_iteracion6.js
+++ loops/loops_iteracion6.js
@@ -1,6 +1,17 @@
 const toys = [{id: 5, name: 'Buzz MyYear'},{id: 11, name: 'Action Woman'},{id: 23, name: 'Barbie Man'},
             {id: 40, name: 'El gato con Guantes'},{id: 40, name: 'El gato felix'}];
+const new_list=[]
 for (let toy of toys){
-    if (toy.id==40 || toy.id==34){
-        toys.splice(toys.indexOf(toy),1)}};
+    if (toy.name.includes('gato')==false){
+        new_list.push(toy)}};
+toys.splice(0,toys.length)
+for (e of new_list){toys.push(e)}
 console.log(toys)
+
+//Esto no me funciona no sé por qué.
+const toys1 = [{id: 5, name: 'Buzz MyYear'},{id: 11, name: 'Action Woman'},{id: 23, name: 'Barbie Man'},
+            {id: 40, name: 'El gato con Guantes'},{id: 40, name: 'El gato felix'}];
+for (let toy of toys1){
+    if (toy.name.includes('gato')){
+        toys1.splice(toys1.indexOf(toy),1)}};
+console.log(toys1)
