3409f30c204d15ad8d587b19c8746487738501b7
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-01-11 19:53:17 +0100

    iteracion 4

--- basics/basic4.js
+++ basics/basic4.js
@@ -1,4 +1,7 @@
 function findArrayIndex(array, text,i=-1) {
+    if (typeof(text)=='number'){text=text.toString()}
+    for (e of array){i++;if (isNaN(e)==false){array[i]=array[i].toString()}}
+    i=-1
     for (e of array){i++;array[i]=array[i].toLowerCase()}
     const index = array.indexOf(text.toLowerCase())
     if (index!=-1){return index}
