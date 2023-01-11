e32647e4d6ab880289da59e3cb71baaf42c06be7
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-01-11 19:13:00 +0100

    iteracion 3

--- basics/basic3.js
+++ basics/basic3.js
@@ -1,9 +1,3 @@
-//Iteración #3: Mix Fors**
-
-//Dado el siguiente javascript usa forof y forin para saber cuantas veces ha sido cada sonido agregado por los usuarios a favorito.
-// Para ello recorre la lista de usuarios y usa forin para recoger el nombre de los sonidos que el usuario tenga como favoritos.
-//Una vez accedas a ellos piensa en la mejor forma de hacer un conteo de cada vez que ese sonido se repita como favorito en cada usuario.
-
 const users = [
     {name: 'Manolo el del bombo',
         favoritesSounds: {
@@ -37,9 +31,9 @@ const users = [
 let i=0
 const lis=[],ulis=[],flis=[],list=[],nlis=[]
 for (obj of users) {
-    for (cat in obj){                                                   //index == name,favoritessounds         users[users.indexOf(obj)]
-        if (cat=='favoritesSounds'){                                    //cat   == favoritesSound               users[users.indexOf(obj)][cat]
-            for (t in (users[users.indexOf(obj)][cat])){                //t     == waves,wind,firecamp          users[users.indexOf(obj)][cat][t]
+    for (cat in obj){                                                   
+        if (cat=='favoritesSounds'){                                    
+            for (t in (users[users.indexOf(obj)][cat])){                
                 lis.push(t)}}}}
 for (sound of lis.sort()){i++;if (sound!=lis[i]){ulis.push(sound);nlis.push(i)}}
 i=-1
