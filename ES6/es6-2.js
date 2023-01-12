7c862ac521e9728aaeae99e517fc6c9bb87558f4
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-01-12 13:42:18 +0100

    iteracion2

--- ES6/es6-2.js
+++ ES6/es6-2.js
@@ -5,9 +5,7 @@ console.log(title,gender,year)
 
 //2.2
 const fruits = ['Banana', 'Strawberry', 'Orange'];
-const fruit1=fruits.filter(fruit => fruit==fruits[0]),
-      fruit2=fruits.filter(fruit => fruit==fruits[1]),
-      fruit3=fruits.filter(fruit => fruit==fruits[2]);
+const [fruit1,fruit2,fruit3]=fruits
 console.log(fruit1,fruit2,fruit3)
 
 const splitList=(list,flis=[])=>{for (elem of list){flis.push(list.filter(sb => sb==elem))}return flis}
@@ -21,7 +19,5 @@ console.log(name, race)
 //2.4
 const car = {name1: 'Mazda 6', itv: [2015, 2011, 2020] }
 const {name1,itv}=car
-const itv1=[itv[0]],
-      itv2=[itv[1]],
-      itv3=[itv[2]];
+const [itv1,itv2,itv3]=itv
 console.log(name1,itv1,itv2,itv3)
