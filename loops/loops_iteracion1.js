0d402b436dae57c2aacbc756926c8d8e0959e691
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-01-12 16:31:50 +0100

    conversion a arrowf

--- loops/loops_iteracion1.js
+++ loops/loops_iteracion1.js
@@ -1,5 +1,4 @@
 //1
 const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta']
-for (p of products){
-    if (p.toLowerCase().includes("camiseta")){
-        console.log(p)}};
+const findWithWord=(list,word)=>{for (p of list){if (p.toLowerCase().includes(word)){console.log(p)}}}
+findWithWord(products,'camiseta')
