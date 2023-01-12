c7daecc0571b420832799dad98f00426ccf756ce
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-01-12 16:34:33 +0100

    conversion a arrowf

--- loops/loops_iteracion2.js
+++ loops/loops_iteracion2.js
@@ -4,8 +4,8 @@ const alumns=[
 		{name: 'Juan Miranda', T1: false, T2: true, T3: true},
 		{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
 		{name: 'Raquel Benito', T1: true, T2: true, T3: true}];
-for (a of alumns){
+const checkAproved=(list)=>{for (a of alumns){
     if (a.T1 && a.T2 || a.T1 && a.T3 || a.T2 && a.T3){
-        a.isAproved=true}
-    else {a.isAproved=false}};
-console.log(alumns)
+            a.isAproved=true}
+        else {a.isAproved=false}}};
+checkAproved(alumns)
