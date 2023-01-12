a4028e9866492b8d30a087a0726207bc75275483
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-01-12 17:59:09 +0100

    padawan4

---
+++ ejercicios extra/1.padawan/padawan4.js
@@ -0,0 +1,7 @@
+const products = [
+    { name: "Funko Dr. Strange", sellCount: 10 },
+    { name: "Mochila de protones: Ghostbusters", sellCount: 302 },
+    { name: "Sable laser FX", sellCount: 23 },
+    { name: "Varita de Voldemort", sellCount: 6 },];
+const sumSells=(list,c=0)=>{for (product of list){c+=product.sellCount}return c}
+console.log(sumSells(products))
