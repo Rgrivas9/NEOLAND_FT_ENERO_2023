ec2f91b0d15491dcc63cd41a3bedee7b3a834412
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-01-12 20:05:32 +0100

    jedik2

---
+++ ejercicios extra/2.jedi-knight/jedik2.js
@@ -0,0 +1,39 @@
+const users = [
+    {
+      name: "Alberto",
+      favoritesSounds: {
+        waves: { format: "mp3", volume: 50 },
+        rain: { format: "ogg", volume: 60 },
+        firecamp: { format: "mp3", volume: 80 },
+      },
+    },
+    {
+      name: "Antonio",
+      favoritesSounds: {
+        waves: { format: "mp3", volume: 30 },
+        shower: { format: "ogg", volume: 55 },
+        train: { format: "mp3", volume: 60 },
+      },
+    },
+    {
+      name: "Pedro",
+      favoritesSounds: {
+        shower: { format: "mp3", volume: 50 },
+        train: { format: "ogg", volume: 60 },
+        firecamp: { format: "mp3", volume: 80 },
+      },
+    },
+    {
+      name: "Cristina",
+      favoritesSounds: {
+        waves: { format: "mp3", volume: 67 },
+        wind: { format: "ogg", volume: 35 },
+        firecamp: { format: "mp3", volume: 60 },
+      },
+    },];
+
+const averageVol=(list,i=0,acc=0)=>{for (obj of list){
+    for (sound in obj.favoritesSounds){
+        i++;acc+=obj.favoritesSounds[sound].volume}}
+    return acc/i}
+console.log(averageVol(users))
