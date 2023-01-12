b28a55f0a7190423af5b1866a99f7658ed4ba4c5
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-01-12 21:19:17 +0100

    jedik4a

--- ejercicios extra/2.jedi-knight/jedik4a.js
+++ ejercicios extra/2.jedi-knight/jedik4a.js
@@ -7,7 +7,7 @@ const mainCharacters = [
     "Anakin",6,
     "Obi-Wan",];
   
-  const findIndexOf=(list,word,m=0)=>{for(w of list){
+const findIndexOf=(list,word)=>{for(w of list){
     if (typeof(word)=='string' && typeof(w)!='number'){if (w.toLowerCase()==word.toLowerCase()){return list.indexOf(w)}}
     if (typeof(word)!='string'){if (w==word){return list.indexOf(w)}}}
     return false}
