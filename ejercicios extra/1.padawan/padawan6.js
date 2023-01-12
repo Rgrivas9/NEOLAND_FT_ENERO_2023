ad1d916ddd19b5adab8d54826784420966feb209
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-01-12 18:10:31 +0100

    padawan6

--- ejercicios extra/1.padawan/padawan6.js
+++ ejercicios extra/1.padawan/padawan6.js
@@ -2,10 +2,10 @@ const products = [
     { name: "Funko Dr. Strange", sellCount: 10 },
     { name: "Mochila de protones: Ghostbusters", sellCount: 302 },
     { name: "Sable laser FX", sellCount: 23 },
-    { name: "Varita de Voldemort", sellCount: 6 },
-  ];
-  const splitThings=(list,bad=[],good=[])=>{for (thing of list){
+    { name: "Varita de Voldemort", sellCount: 6 },];
+    
+const splitThings=(list,bad=[],good=[])=>{for (thing of list){
       if (thing.sellCount>20){good.push(thing)}
       if (thing.sellCount<=20){bad.push(thing)}}return [good,bad]}
-  const goodProducts = splitThings(products)[0];
-  const badProducts = splitThings(products)[1];
+const goodProducts = splitThings(products)[0];
+const badProducts = splitThings(products)[1];
