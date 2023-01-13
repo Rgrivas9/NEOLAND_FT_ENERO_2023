49f349f16c6e86c293d4b5162db65daa48c6c9fc
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-01-13 10:48:19 +0100

    json1

--- json-practice/json1.js
+++ json-practice/json1.js
@@ -4,8 +4,13 @@ const chars=data.results
 const findThings=(list,char)=>{
     return list.filter(chars=>(chars.name.toLowerCase()).includes(char.toLowerCase()))}
 
-const findAny=(list,charList=[],lis=[])=>{
+const findAny=(list,charList=0,lis=[])=>{
+    if (charList==[]){return listCharNames(list)}
     if (typeof(charList)=='string'){charList=[charList]}
     for (char of charList){lis.push(list.filter(chars=>(chars.name.toLowerCase()).includes(char.toLowerCase())))}
 return(lis)}
-console.log(findAny(chars,['Rick Sanchez','beth smith']))
+
+const listCharNames=(list)=>{return list.map(character=>character.name)}
+console.log(findAny(chars))
+console.log(findAny(chars,'rick sAnchez'))
+console.log(findAny(chars,['Ants in my Eyes JohnSon','beth']))
