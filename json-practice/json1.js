bd8bbce945be5c0a29aebf71b8a759b851abb9f4
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-01-13 10:49:48 +0100

    json1

--- json-practice/json1.js
+++ json-practice/json1.js
@@ -1,8 +1,9 @@
 //char.json==https://rickandmortyapi.com/api/character
 const data=require("./char.json");
 const chars=data.results
+
 const findThings=(list,char)=>{
-    return list.filter(chars=>(chars.name.toLowerCase()).includes(char.toLowerCase()))}
+    return list.filter(chars=>(chars.name.toLowerCase()).includes(char.toLowerCase()))} 
 
 const findAny=(list,charList=0,lis=[])=>{
     if (charList==[]){return listCharNames(list)}
@@ -11,6 +12,7 @@ const findAny=(list,charList=0,lis=[])=>{
 return(lis)}
 
 const listCharNames=(list)=>{return list.map(character=>character.name)}
+
 console.log(findAny(chars))
 console.log(findAny(chars,'rick sAnchez'))
 console.log(findAny(chars,['Ants in my Eyes JohnSon','beth']))
