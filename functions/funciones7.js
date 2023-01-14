4ecedef42bd0803e4e6c543400509a55df35758b
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-01-14 17:43:14 +0100

    funciones7

--- functions/funciones7.js
+++ functions/funciones7.js
@@ -12,11 +12,15 @@ const nameFinder = [
     'Marc'
   ];
   
-function finderName(param,arg) {
+/* function finderName(param,arg) {
     let n=arg.toUpperCase(),i=-1,x=0
     for (nom of param){
         i++
         param[i]=nom.toUpperCase()
         if (n==param[i]){const x=param.indexOf(param[i])
     if (x!=0){return [true,x]}}}
-    if (x==0){return false}}
+    if (x==0){return false}} */
+
+  
+const finderName=(list,name,x=0)=>{for (obj of list){name.toLowerCase()==obj.toLowerCase() ? x=[true,list.indexOf(obj)] : x=false} return x}
+console.log(finderName(nameFinder,'MaRc'))
