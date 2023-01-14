15fa7f988b2eab10216b37ffb03ffd6cf93be442
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-01-14 19:16:08 +0100

    ES6 5.5 html

---
+++ ES6/es6-5-5/app.js
@@ -0,0 +1,21 @@
+const streamers = [
+  { name: "Rubius", age: 32, gameMorePlayed: "Minecraft" },
+  { name: "Ibai", age: 25, gameMorePlayed: "League of Legends" },
+  { name: "Reven", age: 43, gameMorePlayed: "League of Legends" },
+  { name: "AuronPlay", age: 33, gameMorePlayed: "Among Us" },
+];
+const findStreamer = (i = 0) => {
+  for (const streamer of streamers) {
+    if (
+      streamer.name
+        .toLowerCase()
+        .includes(document.getElementById("name").value.toLowerCase())
+    ) {
+      i += 1;
+      console.log(streamer);
+    }
+  }
+  if (i == 0) {
+    console.log("No hay coincidencias");
+  }
+};
