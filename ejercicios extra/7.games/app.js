f4dbbd5a00dc6517079aacb308cd6d2db3085b4d
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-01-15 23:21:59 +0100

    juego planetas

--- ejercicios extra/7.games/app.js
+++ ejercicios extra/7.games/app.js
@@ -74,5 +74,23 @@ const cardArray = [
       img: "public/exercise-1/uranus.svg",
     },
   ];
-console.log(getRandomArray(cardArray))
-console.log(cardArray)
+/* console.log(getRandomArray(cardArray))
+console.log(cardArray) */
+
+/* function startTimer(duration, display) {
+  var timer = duration, minutes, seconds;
+  setInterval(function () {
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      display.textContent = minutes + ":" + seconds;
+
+      if (--timer < 0) {
+          timer = duration;
+      }
+  }, 1000);
+} */
+const countDown = (n) => {n--}
--- ejercicios extra/7.games/exercise-1.js
+++ ejercicios extra/7.games/exercise-1.js
@@ -72,6 +72,7 @@ const getRandomArray=(list,rList=[])=>{
   return rList}
 let cardArray = getRandomArray(cards)
 const randomize=()=>{cardArray = getRandomArray(cards);row1.innerHTML="";row2.innerHTML="";row3.innerHTML="";printInDocument(cardArray)}
+
 //
 const row1 = document.querySelector("#row1");
 const row2 = document.querySelector("#row2");
@@ -107,23 +108,27 @@ const checkPlanets = () => {
     if (lis.length==2){
     if (lis[0][0].id==lis[1][0].id){lis.splice(1,1);idLis.splice(1,1)}}
     if (lis.length==2){
-        if (lis[0][0].name==lis[1][0].name){s++;scored();tickPlanets(idLis);lis.splice(0,2);idLis.splice(0,2);winner()}
+        if (lis[0][0].name==lis[1][0].name){
+          s++;scored();
+          tickPlanets(idLis);
+          lis.splice(0,2);idLis.splice(0,2);winner()}
         if (lis.length!=0){
             if (lis[0][0].name!=lis[1][0].name){a++;scored();wrongPlanets(idLis);lis.splice(0,2);idLis.splice(0,2)}}
     }
 };
 const scored=()=>{score.innerHTML =`
-<h3>Score:<span data-function="scored">${s}</span></h3>
+<h3>Score:<span data-function="scored">${s}</span></h3><h2 style="float: right">${counter}</h2>
 <h3>Attempts:<span data-function="attempted">${a}</span></h3>
 <button id="reset" onclick="reset()">reset </button>`}
 
 
 const reset=()=>{s=0;a=0;lis.splice(0,2);idLis.splice(0,2);row1.innerHTML="";row2.innerHTML="";row3.innerHTML="";
     score.innerHTML =`
-<h3>Score:<span data-function="scored">0</span></h3>
+<h3>Score:<span data-function="scored">0</span></h3><h2 style="float: right">60</h2>
 <h3>Attempts:<span data-function="attempted">0</span></h3>
 <button id="start" onclick="start(cardArray)">start </button><button id="random" onclick="randomize()">random </button>`;
-printInDocument(cardArray)}
+printInDocument(cardArray)
+clearInterval(intervalID);counter=60}
 
 const start=(list, i = 0)=> {row1.innerHTML="";row2.innerHTML="";row3.innerHTML="";
     for (const item of list) {
@@ -139,9 +144,10 @@ const start=(list, i = 0)=> {row1.innerHTML="";row2.innerHTML="";row3.innerHTML=
       }
     }
     score.innerHTML =`
-    <h3>Score:<span data-function="scored">${s}</span></h3>
+    <h3>Score:<span data-function="scored">${s}</span></h3><h2 style="float: right">${counter}</h2>
     <h3>Attempts:<span data-function="attempted">${a}</span></h3>
-    <button id="reset" onclick="reset()">reset </button>`};
+    <button id="reset" onclick="reset()">reset </button>`;
+    timeOut()};
 
 const getImage=(id)=>{
     const image = cardArray.find(image => image.id==id)
@@ -150,6 +156,8 @@ const getImage=(id)=>{
 const tickPlanets=(list)=>{
     document.getElementById(list[0]).src="tick.png"
     document.getElementById(list[1]).src="tick.png"
+    document.getElementById(list[0]).onclick=false
+    document.getElementById(list[1]).onclick=false
 }
 const wrongPlanets=(list)=>{
     document.getElementById(list[0]).src="planets.png"
@@ -158,7 +166,8 @@ const wrongPlanets=(list)=>{
 const winner=()=>{
     if (s==6){    score.innerHTML =`
     <h1> Congratulations! You win! <h1>
-    <button id="reset" onclick="reset()">Try again</button>`}}
+    <h3> Score: ${s} Failed: ${a}<h3>
+    <button id="reset" onclick="reset()">Try again</button>`;clearInterval(intervalID);counter=60}}
 const music = new Audio('kungfury.mp3')
 const unmute=()=>{music.play();
   music.loop=true
@@ -166,3 +175,28 @@ const unmute=()=>{music.play();
 
 const mute=()=>{music.pause()
   sound.innerHTML='<button onclick="unmute()">sound</button>'}
+
+var intervalID
+let counter=60
+const timeOut=()=>{intervalID=setInterval(count,1000)}
+const count = () => {if (counter>0){counter--;
+  score.innerHTML =`
+  <h3>Score:<span data-function="scored">${s}</span></h3><h2 style="float: right">${counter}</h2>
+  <h3>Attempts:<span data-function="attempted">${a}</span></h3>
+  <button id="reset" onclick="reset()">reset </button>`};if (counter==0){noTime()}}
+const noTime = (i=-1) => {score.innerHTML =`<h1 style = "text-align:center"> Failed! Time ran out! <h1>
+  <h3 style = "text-align:center">Score: ${s}<h3>
+  <button id="reset" onclick="reset()">Try again</button>`;clearInterval(intervalID);counter=60;
+  document.getElementById(1).onclick=false
+  document.getElementById(2).onclick=false
+  document.getElementById(3).onclick=false
+  document.getElementById(4).onclick=false
+  document.getElementById(5).onclick=false
+  document.getElementById(6).onclick=false
+  document.getElementById(7).onclick=false
+  document.getElementById(8).onclick=false
+  document.getElementById(9).onclick=false
+  document.getElementById(10).onclick=false
+  document.getElementById(11).onclick=false
+  document.getElementById(12).onclick=false
+}
